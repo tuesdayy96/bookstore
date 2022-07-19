858c2d440c089af380d6eca8cc7664457203f82e
tuesdayy96 <104114353+tuesdayy96@users.noreply.github.com>
2022-07-19 13:18:20 +0900

    release1.3

--- js/book_db.js
+++ js/book_db.js
@@ -1,3 +1,4 @@
+// 수험서
 $.ajax({
     method: "GET",
     url : "https://dapi.kakao.com/v3/search/book?target=title",
@@ -13,3 +14,125 @@ $.ajax({
     }
     
 })
+
+// 참고서
+$.ajax({
+    method: "GET",
+    url : "https://dapi.kakao.com/v3/search/book?target=title",
+    data : {query: "참고서"},
+    headers : {Authorization :"KakaoAK 0abe6ac39569f3f00d940ada52fddad3"},
+})
+.done(function(data){
+    console.log(data)
+    for(var i in data.documents){
+        $('.referrence .au').eq(i).html(data.documents[i].authors+" <small>지음<small>");
+        $('.referrence .con').eq(i).text(data.documents[i].title);
+        $('.referrence .thumb').eq(i).html("<img src='"+data.documents[i].thumbnail+"'>");
+    }
+    
+})
+
+// 외국어
+$.ajax({
+    method: "GET",
+    url : "https://dapi.kakao.com/v3/search/book?target=title",
+    data : {query: "영어 일본어"},
+    headers : {Authorization :"KakaoAK 0abe6ac39569f3f00d940ada52fddad3"},
+})
+.done(function(data){
+    console.log(data)
+    for(var i in data.documents){
+        $('.foreign .au').eq(i).html(data.documents[i].authors+" <small>지음<small>");
+        $('.foreign .con').eq(i).text(data.documents[i].title);
+        $('.foreign .thumb').eq(i).html("<img src='"+data.documents[i].thumbnail+"'>");
+    }
+    
+})
+
+// 대학교재
+$.ajax({
+    method: "GET",
+    url : "https://dapi.kakao.com/v3/search/book?target=title",
+    data : {query: "대학교재" ,page:'2'},
+    headers : {Authorization :"KakaoAK 0abe6ac39569f3f00d940ada52fddad3"},
+})
+.done(function(data){
+    console.log(data)
+    for(var i in data.documents){
+        $('.university .au').eq(i).html(data.documents[i].authors+" <small>지음<small>");
+        $('.university .con').eq(i).text(data.documents[i].title);
+        $('.university .thumb').eq(i).html("<img src='"+data.documents[i].thumbnail+"'>");
+    }
+    
+})
+
+// 에세이/자서전
+$.ajax({
+    method: "GET",
+    url : "https://dapi.kakao.com/v3/search/book?target=title",
+    data : {query: "에세이"},
+    headers : {Authorization :"KakaoAK 0abe6ac39569f3f00d940ada52fddad3"},
+})
+.done(function(data){
+    console.log(data)
+    for(var i in data.documents){
+        $('.essay .au').eq(i).html(data.documents[i].authors+" <small>지음<small>");
+        $('.essay .con').eq(i).text(data.documents[i].title);
+        $('.essay .thumb').eq(i).html("<img src='"+data.documents[i].thumbnail+"'>");
+    }
+    
+})
+
+// 자기계발
+$.ajax({
+    method: "GET",
+    url : "https://dapi.kakao.com/v3/search/book",
+    data : {query: "자기계발", page:"2"},
+    headers : {Authorization :"KakaoAK 0abe6ac39569f3f00d940ada52fddad3"},
+})
+.done(function(data){
+    console.log(data)
+    for(var i in data.documents){
+        $('.develop .au').eq(i).html(data.documents[i].authors+" <small>지음<small>");
+        $('.develop .con').eq(i).text(data.documents[i].title);
+        $('.develop .thumb').eq(i).html("<img src='"+data.documents[i].thumbnail+"'>");
+    }
+    
+})
+
+// 문구
+$.ajax({
+    method: "GET",
+    url : "https://dapi.kakao.com/v3/search/book",
+    data : {query: "팬시", page:"2"},
+    headers : {Authorization :"KakaoAK 0abe6ac39569f3f00d940ada52fddad3"},
+})
+.done(function(data){
+    console.log(data)
+    for(var i in data.documents){
+        $('.fancy .au').eq(i).html(data.documents[i].authors+" <small>지음<small>");
+        $('.fancy .con').eq(i).text(data.documents[i].title);
+        $('.fancy .thumb').eq(i).html("<img src='"+data.documents[i].thumbnail+"'>");
+    }  
+})
+
+// 오늘의 책
+$.ajax({
+    method: "GET",
+    url : "https://dapi.kakao.com/v3/search/book?target-title",
+    data : {query: "공시",page:"2"},
+    headers : {Authorization :"KakaoAK 0abe6ac39569f3f00d940ada52fddad3"},
+})
+.done(function(data){
+    console.log(data)
+    for(var i in data.documents){
+        $('.todayBook .au').eq(i).html("<small>"+data.documents[i].authors[0]+'/'+data.documents[i].publisher+"</small>");
+        $('.todayBook .title').eq(i).text(data.documents[i].title);
+        $('.todayBook .thumb').eq(i).html("<img src='"+data.documents[i].thumbnail+"'>");
+        var str = data.documents[i].contents;
+        var str2 = str.substring(0,40);
+        $('.todayBook .con').eq(i).html(str2);
+    }
+    
+})
+
--- js/main.js
+++ js/main.js
@@ -30,34 +30,30 @@ function tap_slide(){
     prev1.addEventListener('click',slidePrev);
 }
 tap_slide();
-rollings();
 
 // 롤링 배너
-function rollings(){
+$(function(){
     var banner_left = 0;
     var imgCnt = 0;
     var first = 1;
     var last;
     var interval;
-    $(function(){
-        $('.test li').each(function(idx){
-            $(this).css('left',banner_left);
-            banner_left += $(this).width()+parseInt($(this).css("margin-right"));
-            $(this).attr('class','content'+(++idx));
-        });
-
-        last = imgCnt;
-        rollingStart();
+    $('.test li').each(function(){
+        $(this).css('left',banner_left);
+        banner_left += $(this).width()+parseInt($(this).css("margin-right"));
+        $(this).attr('class','content'+(++imgCnt));
     });
+    last = imgCnt;
+    rollingStart();
     function rollingStart(){
         interval = setInterval(function(){
-            $('.visual_view li').each(function(){
-                // $(this).css('left',$(this).position().left-1);
+            $('.viewport li').each(function(){
+                $(this).css('left',$(this).position().left-1);
             });
             var first_content = $('.content' + first);
             var last_content = $('.content' + last);
             if(first_content.position().left <'-'+($(first_content).width())){
-                first_content.css('left', last_content.position().left+last_content.width()+40);
+                first_content.css('left', last_content.position().left+ last_content.width()+40);
                 first++;
                 last++;
                 if(last>imgCnt){
@@ -69,21 +65,28 @@ function rollings(){
             }
         },20);
     }
-}
+})
+   
+
+
 
 // 마우스오버 & 클릭
-
-tap.onmouseover = function(e){
-    var tap_li = document.querySelectorAll('#tap li');
-    for(var i = 0; i<tap_li.length;i++){
-        tap_li[i].classList.remove('on');
-        e.target.classList.add('on');
-    }
-}
-tap.onclick = function(e){
-    var tap_li = document.querySelectorAll('#tap li');
-    for(var i = 0; i<tap_li.length;i++){
-        tap_li[i].classList.remove('on');
-        e.target.classList.add('on');
-    }
-}
+$('.tap_menu li').on('mouseover',function(){
+    var v_port = $('<div>').addClass('viewport');
+    var idx = $(this).index();
+    $(this).addClass('on');
+    $(this).siblings().removeClass('on');
+    $('.visual_view').unwrap(".viewport").css('visibility','hidden');
+    $('.visual_view').eq(idx).wrap(v_port).css('visibility','visible');
+    var banner_left = 0;
+    var imgCnt = 0;
+    var first = 1;
+    var last;
+    var interval;
+    $('.viewport li').each(function(){
+        $(this).css('left',banner_left);
+        banner_left += $(this).width()+parseInt($(this).css("margin-right"));
+        $(this).attr('class','content'+(++imgCnt));
+    });
+    last = imgCnt;
+})
