8bc383bcdb04b510f8d42297ded23e323cd08fc5
tuesdayy96 <104114353+tuesdayy96@users.noreply.github.com>
2022-07-26 17:59:12 +0900

    release2.4

--- js/sub_db.js
+++ js/sub_db.js
@@ -2,18 +2,29 @@ $(function(){
     $.get('./books.txt',function(data){
         $('.books_desc').html(data);
         
-    $('.probtn').click(function(){
-        if($(this).val()==1){
-            $('.books_desc').toggleClass('active');
-            $(this).parent().toggleClass('active');
-            $(this).html('상품 내용 접기');
-            $(this).prop('value',2);
-        } else {
-            $('.books_desc').toggleClass('active');
-            $(this).parent().toggleClass('active');
-            $(this).html('상품 내용 펼치기');
-        }
-        
+        $('.probtn').click(function(){
+            if($(this).val()==1){
+                $('.books_desc').toggleClass('active');
+                $(this).parent().toggleClass('active');
+                $(this).html('상품 내용 접기');
+                $(this).prop('value',2);
+            } else {
+                $('.books_desc').toggleClass('active');
+                $(this).parent().toggleClass('active');
+                $(this).html('상품 내용 펼치기');
+            }
+        })
     })
+    $.get('./delivery.txt',function(data){
+        $('.pop_delivery .overflow').html(data);
+
+        $('.bar_bottom li').click(function(){
+            var idx = $(this).index();
+            $('.books_pop > div').eq(idx).show();
+            $('.books_pop > div').eq(idx).siblings().hide();
+        })
+        $('.x_btn').click(function(){
+            $(this).parent().hide();
+        })
     })
 })
