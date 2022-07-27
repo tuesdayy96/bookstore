563ab3885d6828be000b04c591a031350b85e103
tuesdayy96 <104114353+tuesdayy96@users.noreply.github.com>
2022-07-27 12:16:03 +0900

    release2.5

--- js/sub.js
+++ js/sub.js
@@ -1,8 +1,11 @@
 // 수량 조절
 var price = 12600;
 var amount = document.getElementById('amount');
-var total = document.querySelector('.total'); 
+var amount2 = document.getElementById('amount2');
+var total = document.querySelector('.total');
+var total2 = document.querySelector('.total2');
 var sum;
+var sum2;
 function add(){
     addcount = amount;
     if(addcount.value >= 99){
@@ -33,6 +36,37 @@ function change(){
     total.innerHTML = '<span>합계</span>'+newSum+'원';
 };
 
+function add2(){
+    addcount = amount2;
+    if(addcount.value >= 99){
+        addcount.value = 99 - 1;
+        alert('최대 99권까지 구매할 수 있습니다.');
+    }
+    addcount.value++;
+    sum2 = parseInt(addcount.value)*price;
+    newSum = sum2.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+    total2.innerHTML = '<span>합계</span>'+'<span>'+newSum+'원'+'</span>';
+}
+function del2(){
+    addcount = amount2;
+    if(addcount.value > 1){
+        addcount.value--;
+        sum2 = parseInt(addcount.value)*price;
+        newSum = sum2.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+        total2.innerHTML = '<span>합계</span>'+'<span>'+newSum+'원'+'</span>';
+    }
+};
+function change2(){
+    addcount = amount2;
+    if(addcount.value < 0){
+        addcount.value = 0;
+    }
+    sum2 = parseInt(addcount.value)*price;
+    newSum = sum2.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+    total2.innerHTML = '<span>합계</span>'+'<span>'+newSum+'원'+'</span>';
+};
+
+
 // 팝업
 $(function(){
     $('.closeup').click(function(){
@@ -41,9 +75,9 @@ $(function(){
     $('.popclose, .imgPop img').click(function(){
         $('.imgPop').stop().hide();
     })
-    $('#amount').on('keyup',function(){
+    $('#amount, #amount2').on('keyup',function(){
         this.value = this.value.replace(/[^0-9]/g,'1');
-        if(this.value >= 99 || isNaN(this.value) || this.value == null){
+        if(this.value > 99 || isNaN(this.value) || this.value == null){
            return this.value = 1;
         }
     })
@@ -57,12 +91,12 @@ $(function(){
         } else {
             $('.chx input').prop('checked',false)
         }
-        $('.adds_title span').click(function(){
-            $(this).addClass('on');
-            $(this).siblings().removeClass('on');
-            $('.left strong span').html($(this).text());
-            $('input:checked').prop('checked',false);
-        })
+    })
+    $('.adds_title span').click(function(){
+        $(this).addClass('on');
+        $(this).siblings().removeClass('on');
+        $('.left strong span').html($(this).text());
+        $('input:checked').prop('checked',false);
     })
 })
 
@@ -79,3 +113,18 @@ $(function(){
     })
 })
 
+$(function(){
+    $('.sns li').on('mouseenter',function(){
+        $(this).addClass('on');
+    })
+    $('.sns li').on('mouseleave',function(){
+        $(this).removeClass('on');
+    })
+})
+
+var to_r = document.querySelector('.to_review');
+function reviewScroll(){
+    var r_scroll = document.querySelector('.review').offsetTop;
+    window.scrollTo({top:r_scroll,behavior:'smooth'});    
+}
+to_r.addEventListener('click',reviewScroll);
